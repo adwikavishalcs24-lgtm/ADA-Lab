4f926070bf5722a4976646a679cd7ab797dcdef7
adwikavishalcs24-lgtm <adwikavishal.cs24@bmsce.ac.in>
2026-05-04 09:46:26 +0530

    Add files via upload

---
+++ Floyd.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define V 4
+#define INF 99999
+
+void floyd(int graph[V][V]) {
+    int dist[V][V];
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    for (int k = 0; k < V; k++)
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V];
+    printf("Enter the adjacency matrix (use %d for INF):\n", INF);
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            scanf("%d", &graph[i][j]);
+
+    floyd(graph);
+    return 0;
+}
